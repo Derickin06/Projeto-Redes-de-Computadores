027f100c2f3c39934814a469fcd64549fb7de9a9
Derickin06 <derikinho006@gmail.com>
2026-04-09 20:00:25 -0300

    Implement UDP server to receive messages
    Added server functionality to the UDP socket, including binding to an IP and port, and receiving messages.

--- udp_server.py
+++ udp_server.py
@@ -1,15 +1,22 @@
-import socket  #importa modulo socket
-  
-IP_destino = "192.168.0.3"  #Endereço IP do servidor
-PORTA_destino = 5005          #Numero de porta do servidor
-MENSAGEM = "Hello, World!"
- 
-print ("Endereço IP de destino:", IP_destino)
-print ("Porta UDP de destino:", PORTA_destino)
-print ("Mensagem enviada:", MENSAGEM)
- 
-#Criação de socket UDP
-sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+import socket
 
-#Envia mensagem usando socket UDP
-sock.sendto(MENSAGEM.encode('UTF-8'), (IP_destino, PORTA_destino))
+IP_servidor = "192.168.0.3" #endereço onde o Server será executado
+PORTA_servidor = 5005       #porta aberta pelo Server para conexão
+
+# Criação de socket UDP
+# Argumentos, AF_INET que declara a família do protocolo; se fosse um envio via Bluetooth usariamos AF_BLUETOOTH
+# SOCK_DGRAM, indica que será UDP.
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM) 
+
+# IP e porta que o servidor deve aguardar a conexão
+sock.bind((IP_servidor, PORTA_servidor)) 
+ 
+while True:
+    # Recebe mensagem via socket sock.recvform
+    # aloca 1024 bytes
+    #separa dados e armazena em data e o endereço de origem e guarda em addr
+    data, addr = sock.recvfrom(1024)
+    #imprime endereço do cliente
+    print("Mensagem recebida de : ",addr)
+    #exibe texto enviado pelo cliente
+    print ("Mensagem recebida:", data)
